e1c7b785e9f796d81b9f5ed03bb889e6623597d2
ayush00git <ayush.2007.iit@gmail.com>
2025-05-15 14:33:31 +0530

    Initial commit, learning DOM but a lot of confused and exhanusted as it's first time learning this one and there are still 7 more concepts like this to go

---
+++ script.js
@@ -0,0 +1,65 @@
+const view1 = document.querySelector("#view1")
+console.log(view1)
+
+const view2 = document.getElementById("view2")
+console.log(view2) 
+
+view1.style.display = ""
+view2.style.display = "flex"
+
+const views = document.getElementsByClassName("view")
+const sameViews = document.querySelectorAll(".view")
+console.log(views)
+console.log(sameViews)
+
+const divs = view1.querySelectorAll("div")
+console.log(divs)
+
+const sameDivs = view1.getElementsByTagName("div")
+console.log(sameDivs)
+
+const evenDivs = view1.querySelectorAll("div:nth-of-type(2n)")
+console.log(evenDivs)
+
+for(let i = 0; i < evenDivs.length; i++){
+    evenDivs[i].style.backgroundColor = "green";
+}
+
+const navText = document.querySelector("nav h1")
+console.log(navText)
+
+navText.style.color = "yellow"
+navText.textContent = "Hello World!"
+
+const nav = document.querySelector("nav")
+console.log(nav)
+nav.innerHTML = `<h1>Hello!</h1> <p>This is the navbar content</p>`
+
+nav.style.justifyContent = "flex-start"
+nav.style.justifyContent = "flex-end"
+nav.style.justifyContent = "space-evenly"
+nav.style.justifyContent = "space-between"
+
+// navigating inside a DOM tree
+console.log(evenDivs[0])
+console.log(evenDivs[0].parentElement)
+console.log(evenDivs[0].parentElement.children)
+console.log(evenDivs[0].parentElement.childNodes)
+evenDivs[0].textContent = "Ayush"
+// to check that do a parentElement contains childNodes or not
+console.log(evenDivs[0].parentElement.hasChildNodes())
+
+console.log(evenDivs[0].parentElement.lastChild)
+console.log(evenDivs[0].parentElement.lastElementChild)
+console.log(evenDivs[0].parentElement.firstChild)
+console.log(evenDivs[0].parentElement.firstElementChild)
+
+console.log(evenDivs[0].nextSibling)
+console.log(evenDivs[0].nextElementSibling)
+console.log(evenDivs[0].previousSibling)
+console.log(evenDivs[0].previousElementSibling)
+
+// accessing the div 7 
+console.log(evenDivs[2].nextElementSibling)
+console.log(evenDivs[3].previousElementSibling)
+console.log(evenDivs[1].nextElementSibling.nextElementSibling.nextElementSibling)
